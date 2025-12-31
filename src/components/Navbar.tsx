2d3e114d1df7c04fc26a89c511d963e22f4800e5
Hashimai7() <muhammadhashimai7@gmail.com>
2025-12-31 01:40:53 -0800

    hashim

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Navbar = () => {
   return (
-    <div className="flex gap-4 p-4 bg-gray-200 dark:bg-gray-800 text-lg font-semibold">
+    <div className="flex gap-4 p-4 bg-red-200 dark:bg-gray-800 text-lg font-semibold">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
